782f621cbdaf074af9da1597112563a08c95f065
Neural Network Industries <39800143+austriachainone@users.noreply.github.com>
2024-01-16 21:12:45 +0100

    Add files via upload

---
+++ tileset2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="tileset2" tilewidth="32" tileheight="32" tilecount="1365" columns="39">
+ <image source="tileset2.png" width="1248" height="1120"/>
+</tileset>
